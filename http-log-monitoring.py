fb4065fbcdbf10c078d918fc7f270ecd25c50154
pli01 <pli01@noreply.github.com>
2020-12-20 21:42:00 +0100

    Fix http-log-monitoring

--- http-log-monitoring.py
+++ http-log-monitoring.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+   HTTP log monitoring console program
    Read log file "filename" , display stats at regular interval, and display alert when threshold is exceeded
 """
 
@@ -126,7 +127,7 @@ def parse_clf_http_line(line, stats_data):
     for key, value in entry:
         if key not in stats_data:
             stats_data[key] = Counter()
-        if key is 'size':  # increment total bytes size
+        if key == 'size':  # increment total bytes size
             stats_data[key].update({key: int(data['size'])})
         else:
             stats_data[key].update([value])
@@ -154,36 +155,37 @@ def main(filename, stats_interval, threshold, alarm_interval):
                     if line:
                         stats_data = parse_clf_http_line(line, stats_data)
                         total_req_count += 1
-                    else:
-                        try:
-                            if file.tell() > os.path.getsize(filename):  # rotation occurred (copytruncate/create)
-                                file.close()
-                                seek_end = False
-                                break
-                            pos = file.tell()
-                            file.seek(pos)
-                            time.sleep(1)
-                            stats_timer += 1
-                            alarm_timer += 1
+                        continue
 
-                            if stats_timer == stats_interval:
-                                display_summary_stats(
-                                    now, stats_interval, stats_data)
-                                alarm_total_req_count += total_req_count
-                                stats_data = {}
-                                total_req_count = 0
-                                stats_timer = 0
+                    try:
+                        if file.tell() > os.path.getsize(filename):  # rotation occurred (copytruncate/create)
+                            file.close()
+                            seek_end = False
+                            break
+                        pos = file.tell()
+                        file.seek(pos)
+                        time.sleep(1)
+                        stats_timer += 1
+                        alarm_timer += 1
 
-                            if alarm_timer == alarm_interval:
-                                alert_sent = alarm_threshold(
-                                    now, alarm_total_req_count, alarm_interval, threshold, alert_sent)
-                                alarm_total_req_count = 0
-                                alarm_timer = 0
+                        if stats_timer == stats_interval:
+                            display_summary_stats(
+                                now, stats_interval, stats_data)
+                            alarm_total_req_count += total_req_count
+                            stats_data = {}
+                            total_req_count = 0
+                            stats_timer = 0
 
-                        except FileNotFoundError:  # rotation occurred but new file still not created
-                            print('{filename} is still not ready for reading'.format(
-                                filename=filename))
-                            time.sleep(1)
+                        if alarm_timer == alarm_interval:
+                            alert_sent = alarm_threshold(
+                                now, alarm_total_req_count, alarm_interval, threshold, alert_sent)
+                            alarm_total_req_count = 0
+                            alarm_timer = 0
+
+                    except FileNotFoundError:  # rotation occurred but new file still not created
+                        print('{filename} is still not ready for reading'.format(
+                            filename=filename))
+                        time.sleep(1)
 
         except IOError:
             print('Unable to open {filename} for reading'.format(
